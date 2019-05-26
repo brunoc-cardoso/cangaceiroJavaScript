41073cf41aecc115eda0bfd4f647868a1a7117b3
Bruno <brunocpessoal@gmail.com>
2019-05-25 22:17:32 -0300

    finalized the creation of the negotiation model

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,25 +1,25 @@
 class Negocicao {
 
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        Object.assign(this, {_quantidade, _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
     }
 
-    obtemVolume() {
+    getVolume() {
         return this._quantidade * this._valor;
     }
 
     // métodos acessores
-    getData() {
-        return this._data;
+    get data() {
+        return new Date(this._data.getTime());
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
