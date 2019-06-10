3a2c666c181441ae2f8df40df2528cad9dc86f5a
bruno.cardoso <brunocpessoal@gmail.com>
2019-06-10 15:16:02 -0300

    Applied the Proxy project pattern

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -8,8 +8,22 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._negociacoes = new Negociacoes(model => {
-      this._negociacoesView.update(model);
+    const self = this;
+
+    this._negociacoes = new Proxy(new Negociacoes(), {
+      get(target, prop, receiver) {
+        if (typeof(target[prop]) == typeof(Function)
+            && ['adiciona', 'esvazia'].includes(prop)) {
+            
+            return function () {
+                console.log(`"${prop}" disparou armadilha`);
+                target[prop].apply(target, arguments);
+                self._negociacoesView.update(target);
+            }
+        } else {
+            return target[prop];
+        }
+      }
     });
 
     this._negociacoesView = new NegociacoesView('#negociacoes');
