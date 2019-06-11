79dbce41efe646e2fcc9679b834c34a2881d0c7f
bruno.cardoso <brunocpessoal@gmail.com>
2019-06-11 14:12:01 -0300

    added exception handling

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -22,10 +22,19 @@ class NegociacaoController {
   }
 
   adiciona(event) {
-    event.preventDefault();
-    this._negociacoes.adiciona(this._criaNegociacao());
-    this._mensagem.texto = 'Negociação adicionada com sucesso';
-    this._limpaFormulario();
+    try {
+      event.preventDefault();
+      this._negociacoes.adiciona(this._criaNegociacao());
+      this._mensagem.texto = 'Negociação adicionada com sucesso';
+      this._limpaFormulario();
+    } catch(err) {
+      console.log(err);
+      if (err instanceof DataInvalidaException) {
+        this._mensagem.texto = err.message;
+      } else {
+        this._mensagem.texto = 'Um erro não esperado aconteceu. Entre em contato com o suporte';
+      }
+    }
   }
 
   _criaNegociacao() {
---
+++ client/app/ui/converters/DataInvalidaException.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class DataInvalidaException extends ApplicationException {
+
+  constructor() {
+    super('A data deve estar no formato dd/mm/aaaa');
+  }
+
+}
