b704fc7d1f0bebb74cd46569c4754d6c7ed4a318
bruno.cardoso <brunocpessoal@gmail.com>
2019-06-11 10:37:15 -0300

    Added Bind class for code simplification

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -8,22 +8,17 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._negociacoes = ProxyFactory.create(
+    this._negociacoes = new Bind(
       new Negociacoes(),
-      ['adiciona', 'esvazia'],
-      model => this._negociacoesView.update(model)
+      new NegociacoesView('#negociacoes'),
+      'adiciona', 'esvazia'
     );
 
-    this._negociacoesView = new NegociacoesView('#negociacoes');
-    this._negociacoesView.update(this._negociacoes);
-    
-    this._mensagem = ProxyFactory.create(
+    this._mensagem = new Bind(
       new Mensagem(),
-      ['texto'],
-      model => this._mensagemView.update(model)
-      );
-    this._mensagemView = new MensagemView('#mensagemView');
-    this._mensagemView.update(this._mensagem);
+      new MensagemView('#mensagemView'),
+      'texto'
+    );
   }
 
   adiciona(event) {
