2298322da5bb1cdf0e55a7aa6295ab175ed5e468
bruno.cardoso <brunocpessoal@gmail.com>
2019-07-29 10:37:22 -0300

    Implemented Module Pattern in ConnectionFactory. (ch. 15.4)

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,39 +1,44 @@
 'use strict';
 
-const stores = ['negociacoes'];
+const ConnectionFactory = (() => {
+  const stores = ['negociacoes'];
+  let connection = null;
 
-class ConnectionFactory {
+  return class ConnectionFactory {
 
-  constructor() {
-    throw new Error('Não é possível criar instâncias dessa classe.');
+    constructor() {
+      throw new Error('Não é possível criar instâncias dessa classe.');
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        if (connection) return resolve(connection);
+        const openRequest = indexedDB.open('jscangaceiro', 2);
+
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result);
+        };
+
+        openRequest.onsuccess = e => {
+          connection = e.target.result;
+          resolve(e.target.result);
+        };
+
+        openRequest.onerror = e => {
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+      });
+    }
+
+    static _createStores(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+
   }
-
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      const openRequest = indexedDB.open('jscangaceiro', 2);
-
-      openRequest.onupgradeneeded = e => {
-        ConnectionFactory._createStores(e.target.result);
-      };
-
-      openRequest.onsuccess = e => {
-        resolve(e.target.result);
-      };
-
-      openRequest.onerror = e => {
-        console.log(e.target.error);
-        reject(e.target.error.name);
-      };
-    });
-  }
-
-  static _createStores(connection) {
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store))
-        connection.deleteObjectStore(store);
-
-      connection.createObjectStore(store, { autoIncrement: true });
-    });
-  }
-
-}
+})();
